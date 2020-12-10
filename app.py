6d9bde3cf7fe06430585b7c313a090ae76b5fff9
apbd <60779371+apbd@users.noreply.github.com>
2020-12-10 22:25:59 +0200

    somewhat works

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from config import Config
-from extensions import db, jwt
+from extensions import db
 
-
-from resources.user import UserListResource, UserResource, MeResource, UserRecipeListResource
-from resources.token import TokenResource, RefreshResource, RevokeResource, black_list
-from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+from resources.user import UserListResource
+from resources.blog import RecipeListResource, RecipeResource, RecipePublishResource
 
 
 def create_app():
@@ -24,27 +22,12 @@ def create_app():
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
-    jwt.init_app(app)
-
-    @jwt.token_in_blacklist_loader
-    def check_if_token_in_blacklist(decrypted_token):
-        jti = decrypted_token['jti']
-        return jti in black_list
 
 
 def register_resources(app):
     api = Api(app)
 
     api.add_resource(UserListResource, '/users')
-    api.add_resource(UserResource, '/users/<string:username>')
-    api.add_resource(UserRecipeListResource, '/users/<string:username>/recipes')
-
-    api.add_resource(MeResource, '/me')
-
-    api.add_resource(TokenResource, '/token')
-    api.add_resource(RefreshResource, '/refresh')
-    api.add_resource(RevokeResource, '/revoke')
-
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
     api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
