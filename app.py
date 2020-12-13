a6acdd6284994273bea59f1520605f8891924eaf
apbd <60779371+apbd@users.noreply.github.com>
2020-12-13 19:17:08 +0200

    schemas added

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from config import Config
-from extensions import db
-from models.user import User
+from extensions import db, jwt
+
+
+from resources.user import UserListResource, UserResource, MeResource
+from resources.token import TokenResource, RefreshResource, RevokeResource, black_list
 from resources.blog import RecipeListResource, RecipeResource, RecipePublishResource
 
 
@@ -23,10 +26,25 @@ def create_app():
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
+    jwt.init_app(app)
+
+    @jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decrypted_token):
+        jti = decrypted_token['jti']
+        return jti in black_list
 
 
 def register_resources(app):
     api = Api(app)
+
+    api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+
+    api.add_resource(MeResource, '/me')
+
+    api.add_resource(TokenResource, '/token')
+    api.add_resource(RefreshResource, '/refresh')
+    api.add_resource(RevokeResource, '/revoke')
 
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
